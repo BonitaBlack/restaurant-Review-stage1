7df06ed77a746c69afbc6ce0954f3f9b5f9190dc
Danelle Black <danelle.black@gmail.com>
2018-10-08 15:12:37 -0700

    added a service worker, although it isn't working at the moment. Fixed inner restaurant responisveness and am working on getting caching to also work.

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -35,7 +35,7 @@ initMap = () => {
   });
 }  
  
- window.initMap = () => {
+ /*window.initMap = () => {
   fetchRestaurantFromURL((error, restaurant) => {
     if (error) { // Got an error!
       console.error(error);
@@ -50,7 +50,7 @@ initMap = () => {
     }
   });
 } 
-
+*/
 /**
  * Get current restaurant from page URL.
  */
